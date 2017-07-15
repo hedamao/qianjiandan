24da00e0746aec63b7137e2b2d236753cef1894c
ferrisBreeze <123532408@qq.com>
2017-07-15 20:41:03 +0800

    '静态页面'

---
+++ static/js/common.js
@@ -0,0 +1,24 @@
+$(function() {
+	//导航
+	$('.nav li').hover(function(){
+		$('span',this).stop().css('height','2px');
+		$('span',this).animate({
+			left:'0',
+			width:'100%',
+			right:'0'
+		},200);
+	},function(){
+		$('span',this).stop().animate({
+			left:'50%',
+			width:'0'
+		},200);
+	});
+  
+	$(".lnav dl").each(function(){
+	  	$(this).find("dd:last-child a").css("border-bottom","none");
+	})
+	$(".zk").click(function(){
+	  	$(this).next(".yhs").toggle()
+	})
+  
+})
